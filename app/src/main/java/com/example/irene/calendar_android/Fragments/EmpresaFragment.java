3f7990f8777e51090da64c7b5eec12822a1d8220
irenefoz <ifoz1996@gmail.com>
2017-05-01 00:02:31 +0200

    Modificaciones varies calendari

--- app/src/main/java/com/example/irene/calendar_android/Fragments/EmpresaFragment.java
+++ app/src/main/java/com/example/irene/calendar_android/Fragments/EmpresaFragment.java
@@ -3,6 +3,8 @@ package com.example.irene.calendar_android.Fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,11 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.irene.calendar_android.CompanyCreation.Creacio_Companyia;
+import com.example.irene.calendar_android.EmpresesLlista;
 import com.example.irene.calendar_android.Home.MainActivity;
 import com.example.irene.calendar_android.R;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -23,9 +30,6 @@ public class EmpresaFragment extends Fragment{
 
     //Definicions dels elements del fragment
     Button btnAceptarCrearEmpresa;
-
-    //SQlite simular en la creacció
-
 
     //Contructor
     public EmpresaFragment() {
@@ -36,12 +40,14 @@ public class EmpresaFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_company_list, container, false);
-        // Inflate the layout for this fragment
 
+        /*AdaptadorEmpreses adaptadorEmpreses = new AdaptadorEmpreses(this, dadesExempleEmpresa);
+        ListView llistat = (ListView)findViewById(R.id.lvCompanyList);
+        llistat.setAdapter(adaptadorEmpreses);*/
+
+        //Boto per la creació de les empresa on et enviarà a un altre fragment
         btnAceptarCrearEmpresa = (Button) v.findViewById(R.id.button);
-
         btnAceptarCrearEmpresa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,14 +59,65 @@ public class EmpresaFragment extends Fragment{
 
 
         return v;
-
     }
 
 
+//Adaptardor per el llistat de les empreses on serveix per simular diferents empreses ja creades
+/*
+class AdaptadorEmpreses extends ArrayAdapter<EmpresesLlista>{
+    /*
+        Cridem a una classe anomenada EmpresesLlista on solament hi ha les seves propietats(NOM EMPRESA, CARRER),
+        més li afegom el layout per visualitzar el contingut de cada fila.
+    */
+
+/*
+
+    public AdaptadorEmpreses(Context context,  EmpresesLlista[] dadesExempleEmpresa) {
+        super(context, R.layout.list_empreses);
+    }
+
+    public View getView (int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.list_empreses, null);
+
+        //Declaració de cada element que hi ha en la fila
+        //Nom empresa
+        TextView lblNomEmpresa = (TextView) item.findViewById(R.id.txtNomEmpresa);
+        lblNomEmpresa.setText(dadesExempleEmpresa[position].getNomEmpresa());
+        //Nom carrer
+        TextView lblNomCarrer = (TextView) item.findViewById(R.id.txtCarrer);
+        lblNomCarrer.setText(dadesExempleEmpresa[position].getNomCarrer());
+        //Imatge
+        ImageView foto = (ImageView)item.findViewById(R.id.imageViewEmpresa);
+        switch (dadesExempleEmpresa[position].getImatge())
+        {
+            case "logoDarkAqua": foto.setImageDrawable(getContext().getResources().getDrawable(R.drawable.darkaqua));
+                break;
+            case "logoApp": foto.setImageDrawable(getContext().getResources().getDrawable(R.drawable.calendar_icon));
+                break;
+        }
+        return (item);
+    }*/
 }
+/*
+    private EmpresesLlista dadesExempleEmpresa[] =
+            new EmpresesLlista[]{
+                    new EmpresesLlista("Empresa 1", "Carrer 1", ""),
+                    new EmpresesLlista("Empresa 2", "Carrer 2", ""),
+                    new EmpresesLlista("Empresa 3", "Carrer 3", ""),
+                    new EmpresesLlista("Empresa 4", "Carrer 4", ""),
+                    new EmpresesLlista("Empresa 5", "Carrer 5", ""),
 
 
 
+            };
+*/
+/*
+     int [] imatges = {
+             R.drawable.darkaqua,
+             R.drawable.calendar_icon
+     };
+*/
 
 
 
