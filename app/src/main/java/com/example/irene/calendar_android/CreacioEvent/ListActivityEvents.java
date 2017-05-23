8ea0300c5326ce6d09c7d97036e1a3f85b973dd0
pagoru <pagoru@gmail.com>
2017-05-23 16:22:36 +0200

    Lists eventos y grupos inicio

--- app/src/main/java/com/example/irene/calendar_android/CreacioEvent/ListActivityEvents.java
+++ app/src/main/java/com/example/irene/calendar_android/CreacioEvent/ListActivityEvents.java
@@ -1,18 +1,20 @@
-package com.example.irene.calendar_android.CreacioGrups;
+package com.example.irene.calendar_android.CreacioEvent;
 
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.MatrixCursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.irene.calendar_android.Companyies.ActivityMostrarInfoEmpresa;
+import com.example.irene.calendar_android.CreacioGrups.ActivityMostrarInfoGrup;
+import com.example.irene.calendar_android.CreacioGrups.Adapter;
 import com.example.irene.calendar_android.Login.ActivityLoading;
 import com.example.irene.calendar_android.R;
 
@@ -22,14 +24,14 @@ import net.darkaqua.apiconnector.Request;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ListActivityGrups extends ListActivity {
+public class ListActivityEvents extends ListActivity {
 
     private Adapter cAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_grups);
+        setContentView(R.layout.activity_list_events);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.icon_flecha));
@@ -45,6 +47,7 @@ public class ListActivityGrups extends ListActivity {
 
     public static final String[] FROM = new String[]{
             "_id",
+            "date_id",
             "group_id",
             "company_uuid",
             "name",
@@ -52,10 +55,11 @@ public class ListActivityGrups extends ListActivity {
     };
     public static final int[] TO = new int[]{
             -1,
-            R.id.row_group_id,
-            R.id.row_group_company_uuid,
-            R.id.txtNomGrup,
-            R.id.txtDescripcioGrup,
+            R.id.idEvent,
+            R.id.idGroup,
+            R.id.uuidCompany,
+            R.id.txtTitolEvent,
+            R.id.txtHoraEvent
     };
 
     private void carregarLlistat(){
@@ -66,7 +70,7 @@ public class ListActivityGrups extends ListActivity {
             final Context context = this;
 
             final JSONObject jsonObject = new JSONObject();
-            apiConnector.GET("User/Groups", jsonObject, new Request() {
+            apiConnector.GET("User/Dates", jsonObject, new Request() {
                 @Override
                 public void Response(Object o) {
                     System.out.println(o.toString());
@@ -79,20 +83,21 @@ public class ListActivityGrups extends ListActivity {
                         @Override
                         public void run() {
                             try{
-                                for(int i = 0; i<res.length(); i ++){
+                                for(int i = 0; i< res.length(); i ++){
                                     JSONObject object = res.getJSONObject(i);
                                     mc.addRow(new Object[]{
                                             i + "",
                                             object.getString("id"),
+                                            object.getString("group_id"),
                                             object.getString("company_uuid"),
-                                            object.getString("name"),
-                                            object.getString("description")
+                                            object.getString("title"),
+                                            object.getString("datetime")
                                     });
 
                                     System.out.println(mc.getCount());
                                     cAdapter = new Adapter(
                                             appCompatActivity,
-                                            R.layout.row_grups,
+                                            R.layout.row_events,
                                             mc,
                                             FROM,
                                             TO,
@@ -106,7 +111,7 @@ public class ListActivityGrups extends ListActivity {
                                                 JSONObject jsonObject1 = res.getJSONObject(0);
                                                 Intent i = new Intent(getApplicationContext(), ActivityMostrarInfoGrup.class);
                                                 i.putExtra("company_uuid", jsonObject1.getString("company_uuid"));
-                                                i.putExtra("group_id", jsonObject1.getString("id"));
+                                                i.putExtra("id", jsonObject1.getString("id"));
                                                 startActivity(i);
 
 
@@ -139,10 +144,11 @@ public class ListActivityGrups extends ListActivity {
     public void onListItemClick(ListView l , View view, int position, long id) {
         try{
             Cursor jsonObject1 = (Cursor) getListAdapter().getItem(position);
-            Intent i = new Intent(getApplicationContext(), ActivityMostrarInfoGrup.class);
+            Intent i = new Intent(getApplicationContext(), ActivityMostrarInfoEvents.class);
 
             i.putExtra("company_uuid", jsonObject1.getString(jsonObject1.getColumnIndexOrThrow("company_uuid")));
             i.putExtra("group_id", jsonObject1.getString(jsonObject1.getColumnIndexOrThrow("group_id")));
+            i.putExtra("date_id", jsonObject1.getString(jsonObject1.getColumnIndexOrThrow("date_id")));
             startActivity(i);
 
 
--- app/src/main/java/com/example/irene/calendar_android/CreacioGrups/Adapter.java
+++ app/src/main/java/com/example/irene/calendar_android/CreacioGrups/Adapter.java
@@ -1,12 +1,13 @@
 package com.example.irene.calendar_android.CreacioGrups;
 
+import android.app.ListActivity;
 import android.database.MatrixCursor;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.SimpleCursorAdapter;
 
 
 public class Adapter extends SimpleCursorAdapter  {
-    public Adapter(AppCompatActivity appCompatActivity, int row_grups, MatrixCursor mc, String[] from, int[] to, int i) {
+    public Adapter(ListActivity appCompatActivity, int row_grups, MatrixCursor mc, String[] from, int[] to, int i) {
         super(appCompatActivity, row_grups, mc, from, to);
     }
 
